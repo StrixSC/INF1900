a45f446eba165eb823eb5f4c4001b44d5b94d70a
root <root@localhost.localdomain>
2019-10-21 19:35:18 -0400

    Nawras: Ajout des classes Moteurs, DEL, Memoire, CAN, et ajustement Timer

--- tp/tp7/exec_dir/main.cpp
+++ tp/tp7/exec_dir/main.cpp
@@ -7,7 +7,7 @@
 #include "../lib_dir/del.h"
 #include "../lib_dir/can.h"
 #include "../lib_dir/memoire_24.h"
-// #include "../lib_dir/moteur.h"
+#include "../lib_dir/moteur.h"
  
 // Comment initier les timers:
 // Timer (MODE , TIMER QUE L'ON VEUT UTILISER, DUREE DU TIMER, PRESCALER)
@@ -28,27 +28,23 @@
 Port b = B;                 //Choix du port pour de la Del
 Del del = Del();            //Init d'un nouvel objet Del
 Bouton bouton = Bouton();   //Init d'un nouvel objet Bouton
-
+Moteur moteur = Moteur();
 Timers t = TCNT1_;
 Mode m = Normal;
 Prescaler p = P1024;
 OutputMode op = HighSet;
-uint8_t duree = 10000;
-
-Timer timer1 = Timer(m, t, duree,p, op);
+// uint8_t duree = 781;
+// Timer timer1 = Timer(m, t, duree,p, op);
 volatile uint8_t minuterieExpiree = 0;
 
-ISR(TIMER1_COMPA_vect){
-    del.allumerDEL(b);
-    timer1.stop();
-}
 
 int main(){
-    timer1.start();
-    DDRD = 0x00;
+    DDRD = 0xFF;
     DDRB = 0xFF;
+    moteur.startEngine();
+
     for(;;){
-       
+        moteur.changeSpeed(50,50);
     }
 
     return 0;
