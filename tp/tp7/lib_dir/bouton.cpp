09777acf015760328bc2f3bc7a80caffdb7dea7f
root <root@localhost.localdomain>
2019-10-22 22:42:13 -0400

    ajout des commentaires pour la classe Moteur

--- tp/tp7/lib_dir/bouton.cpp
+++ tp/tp7/lib_dir/bouton.cpp
@@ -1,45 +1,5 @@
 #include "bouton.h"
 
-
-// void Bouton::click(Mode mode){
-//     switch (mode){
-//         case scrutation:
-//             scrutation();
-//         break;
-
-//         case interrupt:
-//             interrupt();
-//         break;
-//     }
-// }
-
-// void Bouton::scrutation() {
-//     if(debounce()){
-//         click = 1;
-//     }
-//     else {
-//         click = 0;
-//     }
-// }
-
-// void Bouton::interrupt() {
-//     bouton.isr();
-// }
-
-// void Bouton::isr() {
-//     ISR (INT0_vect) {
-//         _delay_ms(30);
-//         if(bouton.debounce()){
-//             click = 1;
-//         }
-//         else{
-//             click = 0; 
-//         }
-//     EIFR |= (1 << INTF0);
-//     }
-// }
-
-
     Bouton::Bouton(){}
     Bouton::~Bouton(){}
 
