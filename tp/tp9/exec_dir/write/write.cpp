05ca34f7876d527280f794b791db249ec54542c3
Johnny <johnny.khoury@polymtl.ca>
2019-11-05 16:45:38 -0500

    Bonne version du write

--- tp/tp9/exec_dir/write/write.cpp
+++ tp/tp9/exec_dir/write/write.cpp
@@ -1,6 +1,7 @@
 #include "../lib_dir/UART.h"
 #include "../lib_dir/includes.h"
 #include "../lib_dir/memoire_24.h"
+#include "del.h"
 
 int main(){
     //nous créons des variables de type de chacune des classes que nous allons utliser
@@ -10,6 +11,9 @@ int main(){
     uint8_t premier_Octet;
     uint8_t second_Octet;
     uint16_t tailleOctet;
+    DDRB=0XFF;
+    Del del;
+    Port port =B;
 
     //La mémoire reçoit le premier octet du uart
     premier_Octet = uart.receptionUART();
@@ -19,13 +23,13 @@ int main(){
 
     //écrire le premier bit dans la mémoire
     memoire.ecriture(position, premier_Octet);
-    _delay_ms(4);//dans la documentation à la page 26, on parle d'un delai de programmation de 3.3ms lorsqu'on veut ecrire dans la memoire, donc nous fixons le delai a 4ms pour etre sur que la donnee est bine ecrite
-    positionInitiale++;//la position de la mémoire augmente de 1, car la premiere case vient d etre utilisée
+    _delay_ms(4); //dans la documentation à la page 26, on parle d'un delai de programmation de 3.3ms lorsqu'on veut ecrire dans la memoire, donc nous fixons le delai a 4ms pour etre sur que la donnee est bine ecrite
+    positionInitiale++; //la position de la mémoire augmente de 1, car la premiere case vient d etre utilisée
     
     //écrire le premier bit dans la mémoire
     memoire.ecriture(positionInitiale, deuxieme_bit);
-    _delay_ms(4);//dans la documentation à la page 26, on parle d'un delai de programmation de 3.3ms lorsqu'on veut ecrire dans la memoire, donc nous fixons le delai a 4ms pour etre sur que la donnee est bine ecrite
-    positionInitiale++;//la position de la mémoire augmente de 1, car la premiere case vient d etre utilisée
+    _delay_ms(4); //dans la documentation à la page 26, on parle d'un delai de programmation de 3.3ms lorsqu'on veut ecrire dans la memoire, donc nous fixons le delai a 4ms pour etre sur que la donnee est bine ecrite
+    positionInitiale++; //la position de la mémoire augmente de 1, car la premiere case vient d etre utilisée
 
     //Taille des octets pour savoir le nombre de fois que la boucle while va s executer pour ecrire
     premier_Octet=premier_Octet<<4; //decalage de 4 bits permettra de faire un or avec le second octet et ainsi avoir une valeur sur 16 bits qui representera la taille des 2 octets
@@ -34,9 +38,10 @@ int main(){
     //boucle pour recevoir et ecrire les donnees dans la memoire
     while(position < tailleOctet){
         //Pour pouvoir transmettre une donnée en mémoire externe, nous devons d'abord en recevoir une. La donnée provient de uart, donc nous devons la recevoir
-        uint16_t transmission = uart.receptionUART(); //transmission est de type uint16_t en raison de la classe memoire_24.h
-        memoire.ecriture(position, &transmission);//ecrit a la position initiale la donnee qui a été recue a partir de uart
-        _delay_ms(4);//dans la documentation à la page 26, on parle d'un delai de programmation de 3.3ms lorsqu'on veut ecrire dans la memoire, donc nous fixons le delai a 4ms pour etre sur que la donnee est bine ecrite
+        uint8_t transmission = uart.receptionUART(); //transmission est de type uint16_t en raison de la classe memoire_24.h
+        memoire.ecriture(position, transmission); //ecrit a la position initiale la donnee qui a été recue a partir de uart
+        _delay_ms(4); //dans la documentation à la page 26, on parle d'un delai de programmation de 3.3ms lorsqu'on veut ecrire dans la memoire, donc nous fixons le delai a 4ms pour etre sur que la donnee est bine ecrite
         position++;
     }
+    del.allumerDel(port);
 }
