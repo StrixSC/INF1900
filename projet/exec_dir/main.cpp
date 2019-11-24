7aea1c0d7bb4a5167785046e838cd06058175dee
Nawras <nawrasmamin@gmail.com>
2019-11-24 16:46:52 -0500

    Nawras: LCD Display fonctionne. Areverifier Boucles && Sonar weird thing with capteur

--- projet/exec_dir/main.cpp
+++ projet/exec_dir/main.cpp
@@ -13,12 +13,17 @@ Fichiers utilisés: Uart.h, includes.h, memoire_24.h, del.h, enums.h, moteur.h
 #include "moteur.h"
 #include "piezo.h"
 #include "can.h"
+#include "lcm_so1602dtr_m_fw.h"
+#include "customprocs.h"
+
 // #include "LightSensor.h"
 #define CAPTEUR1 0b00000001
 #define CAPTEUR2 0b00000010
 #define CAPTEUR3 0b00000100
 #define CAPTEUR4 0b00001000
 #define CAPTEUR5 0b00010000
+#define DEMO_DDR	DDRC // `Data Direction Register' AVR occup� par l'aff.
+#define DEMO_PORT	PORTC // Port AVR occup� par l'afficheur
 
 #define AVGSPEED 60
 #define LOWERAVGSPEED 40
@@ -31,6 +36,7 @@ Fichiers utilisés: Uart.h, includes.h, memoire_24.h, del.h, enums.h, moteur.h
 ////////////////////////////////////////////////////////////////
 //Variables et initiation d'objets necessaire pour le programme
 // Memoire24CXXX mem;
+LCM disp(&DEMO_DDR, &DEMO_PORT);
 UART uart;
 Del del;
 Moteur moteur;
@@ -64,6 +70,11 @@ uint8_t endingLoopSequenceCounter = 0;
 
 ///////////////////////////////////////////////////////////////
 //Definition des fonctions
+void static inline w(void) {
+	cp_wait_ms(2000);
+}
+static void banner(LCM&, char*, uint16_t);
+static void wave(LCM&, uint16_t, uint16_t);
 void seanceInit();
 void detect();
 void followLine();
@@ -92,18 +103,12 @@ int main(){
     DDRB = 0xFF; //mode sortie
     DDRD = 0xFF;
     moteur.startEngine();
-    current = Boucles;
+    current = Test;
     while(true){
         switch(current){
             case Test:
-                moteur.turnLeft(AVGSPEED);
-                _delay_ms(1000);
-                moteur.turnRight(AVGSPEED);
-                _delay_ms(1000);
-                moteur.changeSpeed(AVGSPEED, AVGSPEED);
-                _delay_ms(1000);
-                moteur.reverse(AVGSPEED, AVGSPEED);
-                _delay_ms(1000);
+                disp.write("THE STRIX", 2);
+                w();
             break;
             case FollowLine:
                 //Turn sequence test. Do not delete please - Nawras
@@ -441,4 +446,97 @@ void endingLoopSequence(){
     else if(!C1 && !C2 && !C3 && !C4 && !C5){
         moteur.changeSpeed(NOSPEED, NOSPEED);
     }
+}
+
+/**
+ * Fonction pour d�mo - banni�re de texte.
+ * @param lcm		R�f�rence vers un LCM
+ * @param text		Texte � afficher (jeu de caract�res de l'afficheur)
+ * @param ms_interval	Intervalle en millisecondes entre chaque d�callage
+ */
+static void banner(LCM& lcm, char* text, uint16_t ms_interval) {
+	uint8_t start = 0, stop = 1, index = 0;
+	uint16_t msg_len = cp_strlen(text);
+	char t_char = 0;
+	
+	lcm.clear();
+	cp_wait_ms(ms_interval);
+	
+	while (stop <= msg_len && start < msg_len - 1) {
+		if (stop <= LCM_FW_HALF_CH) {
+			index = LCM_FW_HALF_CH - stop;
+		} else {
+           	++start;
+			index = 0;
+		}
+		
+		t_char = text[stop];
+		text[stop] = 0;
+		lcm.write(text + start, index, stop == msg_len);
+		text[stop] = t_char;
+
+		if (stop != msg_len) {
+			++stop;
+		}
+		cp_wait_ms(ms_interval);
+	}
+	
+	lcm.clear();
+	cp_wait_ms(ms_interval);
+}
+
+/**
+ * Fonction pour d�mo - vague.
+ *
+ * @param lcm		R�f�rence vers un LCM
+ * @param rep		Nombre de r�p�titions
+ * @param ms_interval	Intervalle en millisecondes entre chaque mouvement
+ */
+static void wave(LCM& lcm, uint16_t rep, uint16_t ms_interval) {
+	uint8_t t_per [8];
+	
+	for (uint8_t i = 0; i < 8; ++i) {
+		for (uint8_t j = 0; j < 8; ++j) {
+			if (j == i) {
+				t_per[j] = 0xff;
+			} else {
+				t_per[j] = 0;
+			}
+		}
+		lcm.build_cc(i, t_per);
+	}
+	
+	char custom_l1 [] = "\x20\x07\x05\x04\x03\x02\x02\x01\x01\x01\x02\x02"
+		"\x03\x04\x05\x07\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20"
+		"\x20\x20\x20\x20\x20\x20\x07\x05\x04\x03\x02\x02\x01\x01\x01"
+		"\x02\x02\x03\x04\x05\x07";
+	char custom_l2 [] = "\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20"
+		"\x20\x20\x20\x20\x20\x08\x02\x03\x04\x05\x05\x06\x06\x06\x05"
+		"\x05\x04\x03\x02\x08\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20"
+		"\x20\x20\x20\x20\x20\x20";
+
+	char t_char;
+	
+	lcm.clear();
+	
+	for (uint16_t k = 0; k < rep; ++k) {
+		for (uint8_t i = 0; i < LCM_FW_TOT_CH; ++i) {
+			t_char = custom_l1[i + 16];
+			custom_l1[i + LCM_FW_HALF_CH] = 0;
+			
+			lcm.write(custom_l1 + i, 0);
+			
+			custom_l1[i + LCM_FW_HALF_CH] = t_char;
+			t_char = custom_l2[i + LCM_FW_HALF_CH];
+			custom_l2[i + LCM_FW_HALF_CH] = 0;
+			
+			lcm.write(custom_l2 + i, 16);
+			
+			custom_l2[i + LCM_FW_HALF_CH] = t_char;
+			
+			cp_wait_ms(ms_interval);
+		}
+	}
+	
+	lcm.clear();
 }
